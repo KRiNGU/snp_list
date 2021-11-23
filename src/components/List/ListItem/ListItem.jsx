f48f2691effe9fb1672b285aa3a6a967c323556e
KRiNGU <alexandr.lobashov@mail.ru>
2021-11-23 21:57:55 +0300

    fix icon on item__change button

--- src/components/List/ListItem/ListItem.jsx
+++ src/components/List/ListItem/ListItem.jsx
@@ -1,5 +1,6 @@
 import { memo } from "react";
 import './ListItem.css';
+import {MdOutlineOpenInFull} from 'react-icons/md';
 
 const ListItem = ({id, name, phoneNumbers, placeOfResidence}) => {
     return(
@@ -8,7 +9,7 @@ const ListItem = ({id, name, phoneNumbers, placeOfResidence}) => {
             <div className="item__property name">{name}</div>
             <div className="item__property phoneNumbers">{phoneNumbers}</div>
             <div className="item__property placeOfResidence">{placeOfResidence}</div>
-            <button className="item__change" onClick={() => alert()}>change</button>
+            <button className="item__change"><MdOutlineOpenInFull /></button>
         </li>
     );
 };
